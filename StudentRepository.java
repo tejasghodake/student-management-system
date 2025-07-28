e98f0c0e849636ed497e6723c210c8d6ee1c66b4
tejasghodake <tejasghodake24@gmail.com>
2025-07-28 20:27:22 +0530

    Add files via upload

---
+++ StudentRepository.java
@@ -0,0 +1,8 @@
+
+package com.tejas.student.repository;
+
+import com.tejas.student.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
