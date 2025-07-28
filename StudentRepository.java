54d50ed53ad00835b14980dba7c12394320208b9
tejasghodake <tejasghodake24@gmail.com>
2025-07-28 20:54:06 +0530

    Deleted StudentRepository.java

--- StudentRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.tejas.student.repository;
-
-import com.tejas.student.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
